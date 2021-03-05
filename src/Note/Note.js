c6ac41fc1b7a18e2278a2a169e22efbcb29e97fd
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-05 19:28:15 +0000

    updated code with styled-components

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,30 +1,60 @@
 import React from "react";
+import styled from "styled-components";
 
 import Nav from "./Nav";
 import Link from "./Link";
 import View from "./View";
 
+const NoteWrapper = styled.div`
+background: tomato;
+  left:0;
+  right: 0;
+  bottom: 0,
+  top: 0;
+  margin: 0 auto;
+  height: 100vh;
+`;
+
+const DesktopView = styled.div`
+left:0;
+right: 0;
+bottom: 0,
+top: 0;
+display: flex;
+justify-content: center;
+  @media (max-width: 560px) {
+    display: none;
+  }
+`;
+
+const MobileView = styled.div`
+  @media (min-width: 560px) {
+    display: none;
+  }
+`;
+
+const LinkWrapper = styled.div`
+  margin: 0 auto;
+  width: 80%;
+`;
+
 const Note = () => {
   return (
-    <>
-      <div className="d-none d-sm-block">
-        <div className="desktop text-center ">
-          <div className=" display-4 ">
-            <span role="img" aria-label="String">
-              📱
-            </span>
-            <span className="font-italic">only</span>
-          </div>
-        </div>
-      </div>
+    <NoteWrapper>
+      <DesktopView>
+        <span role="img" aria-label="String">
+          📱
+        </span>
+        <span className="font-italic">only</span>
+      </DesktopView>
 
-      <div className="d-none d-block d-sm-none">
+      <MobileView>
         <Nav />
-        <div className="container col-12 col-md-5 ">
+        <LinkWrapper>
           <Link />
-        </div>
-      </div>
-    </>
+        </LinkWrapper>
+      </MobileView>
+    </NoteWrapper>
   );
 };
 
